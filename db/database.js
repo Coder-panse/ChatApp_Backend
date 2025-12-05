27f2149f1d369beb7eb58cb60a35db57f55bedf9
Harshad Panse <gharshadpanse123@gmail.com>
2025-12-05 22:31:36 +0530

    v

--- db/database.js
+++ db/database.js
@@ -1,9 +1,10 @@
 import mongoose from 'mongoose'
 
 function connect(){
-    mongoose.connect("mongodb+srv://gharshadpanse123_db_user:v4w8p24EysVcFn4J@cluster0.xjyhorj.mongodb.net/chatApp")
+    mongoose.connect("mongodb://127.0.0.1:27017/chatApp")
 .then(()=>console.log("Database connected"))
 .catch((err)=>console.log("Database not connected"))
 }
 
 export default connect
+// mongodb+srv://gharshadpanse123_db_user:v4w8p24EysVcFn4J@cluster0.xjyhorj.mongodb.net/
