190be3f07dc8d7c40078e01742b96a6db475f7b2
Harshad Panse <gharshadpanse123@gmail.com>
2025-12-05 21:01:52 +0530

    database

--- routes/user.route.js
+++ routes/user.route.js
@@ -6,6 +6,6 @@ import { upload } from '../config/multerconfig.js'
 userRoute.post("/signup",upload.single("image"),Signup)
 userRoute.post("/login",Login)
 userRoute.get("/logout",Logout)
-userRoute.put("/update/:id",upload.single("uploadImage"),updateProfile)
+
 
 export default userRoute
