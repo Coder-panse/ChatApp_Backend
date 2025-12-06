dc2326e6c13cc5bcb76b18efe10ba2f3d958b490
Harshad Panse <gharshadpanse123@gmail.com>
2025-12-06 18:13:17 +0530

    cors

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ connect();
 
 const app=express()
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://chat-app-frontend-theta-two.vercel.app", credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser())
