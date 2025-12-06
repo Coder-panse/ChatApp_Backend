cfe7f5f1f97039185e9bf54349217a74c6738def
Harshad Panse <gharshadpanse123@gmail.com>
2025-12-06 18:29:04 +0530

    cors in server

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const server = http.createServer(app);
 // Socket Initailize
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://chat-app-frontend-theta-two.vercel.app",
     credentials: true,
   },
 });
